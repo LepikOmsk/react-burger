6bc404d04b3d62e61512a5460188789311c457ea
LepikOmsk <Lepik_a@mail.ru>
2023-02-28 18:34:12 +0300

    добавил redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,23 @@
-import { createStore } from "redux";
-import { rootReducer } from "./reducers";
+import { compose, createStore } from "redux";
 
-export const store = createStore(rootReducer);
+import { combineReducers } from "redux";
+import { burgerConstructorReducer } from "./reducers/burgerConstructorReducer";
+import { currentIngredientReducer } from "./reducers/currentIngredientReducer";
+import { ingredientReducer } from "./reducers/ingredientsReducer";
+import { orderDetailsReducer } from "./reducers/orderDetailsReducer";
+
+const rootReducer = combineReducers({
+  ingredients: ingredientReducer,
+  currentIngredient: currentIngredientReducer,
+  order: orderDetailsReducer,
+  burgerConstructor: burgerConstructorReducer,
+});
+
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers();
+
+export const store = createStore(rootReducer, enhancer);
